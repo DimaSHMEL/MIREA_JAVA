b2f2c9ec4659dd651944abb19c337fc5ba41f292
Dima_SHMEL <55811573+DimaSHMEL@users.noreply.github.com>
2021-09-17 19:56:30 +0300

    labs and test_func update

--- MIREA_JAVA/src/lab_2_2/Human.java
+++ MIREA_JAVA/src/lab_2_2/Human.java
@@ -1,6 +1,38 @@
 package lab_2_2;
 
 public class Human {
+     class Hand {
+        private String orientation;
+        public Hand(String orientation)
+        {
+            this.orientation = orientation;
+        }
+        public String toString() {
+            return "This Hand is " + this.orientation;
+        }
+    }
+     class Head {
+        private String hair_color;
+        public Head()
+        {
+            this.hair_color = "Black";
+        }
+        public String toString()
+        {
+            return "This head has " + this.hair_color + " Hair color";
+        }
+    }
+     class Leg {
+        public String orientation;
+        public Leg(String or)
+        {
+            this.orientation = or;
+        }
+        public String toString()
+        {
+            return "This is " + this.orientation + " leg.";
+        }
+    }
     Head head;
     Hand left_h, right_h;
     Leg left_l, right_l;
@@ -16,6 +48,7 @@ public class Human {
     }
     public Human(String name)
     {
+
         this.head = new Head();
         this.left_h = new Hand("Left");
         this.right_h = new Hand("Right");
@@ -23,55 +56,6 @@ public class Human {
         this.right_l = new Leg("Right");
         this.name = name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Head getHead() {
-        return head;
-    }
-
-    public Hand getLeft_h() {
-        return left_h;
-    }
-
-    public Hand getRight_h() {
-        return right_h;
-    }
-
-    public Leg getLeft_l() {
-        return left_l;
-    }
-
-    public Leg getRight_l() {
-        return right_l;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setHead(Head head) {
-        this.head = head;
-    }
-
-    public void setLeft_h(Hand left_h) {
-        this.left_h = left_h;
-    }
-
-    public void setRight_h(Hand right_h) {
-        this.right_h = right_h;
-    }
-
-    public void setLeft_l(Leg left_l) {
-        this.left_l = left_l;
-    }
-
-    public void setRight_l(Leg right_l) {
-        this.right_l = right_l;
-    }
-
     public String toString() {
         return "Human name is " + this.name + "\n" + this.head + "\n" + this.left_h +
                 "\n" + this.right_h + "\n" + this.left_l + "\n" + this.right_l;
